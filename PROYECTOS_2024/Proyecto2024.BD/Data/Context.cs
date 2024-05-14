73c5716414df0c9806908fe2ffd527a6e48624e6
Leandro Bertero <leandro.bertero@hotmail.com>
2024-05-14 16:44:21 -0300

    Inicio - Primer migracion y actualizacion de la base de datos

---
+++ PROYECTOS_2024/Proyecto2024.BD/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2024.BD.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- PROYECTOS_2024/Proyecto2024.Server/Program.cs
+++ PROYECTOS_2024/Proyecto2024.Server/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Proyecto2024.BD.Data;
+
+
+//---------------------------------------------------------------------
+//configuracion de los servicios en el constructor de la aplicación
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,10 +14,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+//tuve que escribir esta parte manualmente, no me aparecia inicialmente con el proyecto
+builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=conn"));
 
 
 
-
+//---------------------------------------------------------------------
+//construccion de la aplicacion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
