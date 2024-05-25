3964484bad0f3708984ebb879d3776fccb97b7c9
Leandro Bertero <leandro.bertero@hotmail.com>
2024-05-25 16:21:29 -0300

    actualizando el proyeco con comentarios, para entender como se arma todo esto

--- PROYECTOS_2024/Proyecto2024.BD/Data/Entity/Persona.cs
+++ PROYECTOS_2024/Proyecto2024.BD/Data/Entity/Persona.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 namespace Proyecto2024.BD.Data.Entity
 {
     //para unificar los Id hacer un index O INDICE EN RELACION AL ORDEN QUE SE VA A MOSTRAR EN EL FRONTEND
+    //etiquetas de indice, para ordenar los atribitos de las entidades, en cada una de las tablas
     [Index(nameof(TDocumentoId), nameof(NumDoc), Name = "Persona_UQ", IsUnique = true)]
     [Index(nameof(Apellido), nameof(Nombre), Name = "Persona_Apellido_Nombre", IsUnique = false)]
 
@@ -21,7 +22,7 @@ namespace Proyecto2024.BD.Data.Entity
         [MaxLength(12, ErrorMessage = "Máximo número de caracteres {1} ")]
         public string NumDoc { get; set; }
 
-
+        //Requerido o requisito
         [Required(ErrorMessage = "El nombre de la persona es obligatorio")]
         [MaxLength(150, ErrorMessage = "Máximo número de caracteres {1} ")]
         public string Nombre { get; set; }
@@ -30,6 +31,8 @@ namespace Proyecto2024.BD.Data.Entity
         [Required(ErrorMessage = "El apellido de la persona es obligatorio")]
         public string Apellido { get; set; }
 
+
+        //Para hacer las relaciones entre las tablas
         [Required(ErrorMessage = "El tipo de documento es obligatorio")]
         public int TDocumentoId { get; set; }
 
