68716e275c84f54a7809b3fc642f1ffe2c10daa2
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-13 16:43:54 -0300

    Nuestro primer web api

---
+++ MinimalAPIPeliculas/MinimalAPIPeliculas/Entidades/Genero.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalAPIPeliculas.Entidades
+{
+    public class Genero
+    {
+        //Propiedades
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null;
+    }
+}
--- MinimalAPIPeliculas/MinimalAPIPeliculas/Program.cs
+++ MinimalAPIPeliculas/MinimalAPIPeliculas/Program.cs
@@ -1,6 +1,50 @@
+using MinimalAPIPeliculas.Entidades;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+var apellido = builder.Configuration.GetValue<string>("Apellido");
+//inicio de area de los servicios
+
+//fin del area de los servicios
+
 var app = builder.Build();
+
+
+//inicio de area de los middleware
+app.MapGet("/", () => "Hola Mundo");
+
+//para crear un endPoint
+app.MapGet("/generos",  () => {
+ 
+    var generos = new List<Genero>();
+    new Genero
+    {
+        Id = 1,
+        Nombre = "Drama",
+
+    };
+
+    new Genero
+    {
+        Id = 2,
+        Nombre = "Acción",
+
+    };
+
+    new Genero
+    {
+        Id = 3,
+        Nombre = "Comedia",
+
+    };
+
+    return generos; 
+});
+
+
 
 app.MapGet("/", () => "Hello World!");
 
+//fin de area de los middlware
 app.Run();
