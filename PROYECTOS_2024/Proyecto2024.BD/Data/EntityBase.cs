28e4fbd09ebb5f27b4878a6c8b7f9f8e14623497
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-13 00:51:20 -0300

    Comentario

--- PROYECTOS_2024/Proyecto2024.BD/Data/EntityBase.cs
+++ PROYECTOS_2024/Proyecto2024.BD/Data/EntityBase.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Proyecto2024.BD.Data
 {
     //esta clase hereda para el rest de las entidades por la propiedad Id
-    public class EntityBase
+    public class EntityBase //Base de todas las entidades , la cual hereda de las demas entidades por que tiene un Id
     {
         public int Id { get; set; }
 
