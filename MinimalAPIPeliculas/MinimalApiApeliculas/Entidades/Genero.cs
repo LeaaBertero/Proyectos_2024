2db89b7c28f159da1ce7f06fea52f69157828553
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-20 16:05:40 -0300

    probando Core

---
+++ MinimalAPIPeliculas/MinimalApiApeliculas/Entidades/Genero.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace MinimalApiApeliculas.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null;
+    }
+}
--- MinimalAPIPeliculas/MinimalApiApeliculas/Program.cs
+++ MinimalAPIPeliculas/MinimalApiApeliculas/Program.cs
@@ -1,6 +1,48 @@
+using MinimalApiApeliculas.Entidades;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//inicio de area de los servicios
+
+
+var apellido = builder.Configuration.GetValue<string>("apellido");
+
+
+//fin de area de los servicios
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//inicio de area de los middleware
+
+
+app.MapGet("/", () => apellido);
+
+app.MapGet("/generos",() =>
+{
+    var generos = new List<Genero>();
+
+    new Genero
+    {
+        Id = 1,
+        Nombre = "Drama"
+    };
+
+    new Genero
+    {
+        Id = 2,
+        Nombre = "Accion"
+    };
+
+    new Genero
+    {
+        Id = 3,
+        Nombre = "Comedia"
+    };
+
+    return generos;
+
+});
+//fin de area de los middleware
 
 app.Run();
