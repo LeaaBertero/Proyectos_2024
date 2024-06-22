7031050f17a94c2fbfb3e16699c4727028cd51e5
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-21 22:46:54 -0300

    proyecto_TrabajandoEnCapas - Primer parte

---
+++ TrabajandoEnCapas/Entidades/Profesional.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Profesional
+    {
+        private int codProf;
+        private string nombre;
+
+        public Profesional()
+        {
+            codProf = 0;
+            nombre = string.Empty;
+        }
+
+        public int CodProf
+        {
+            get { return codProf; }
+            set { codProf = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
