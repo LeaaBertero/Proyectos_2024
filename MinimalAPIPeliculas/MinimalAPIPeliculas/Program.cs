e72196470859388147ad33a5583a964e6374b6b8
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-25 00:38:18 -0300

    instalando swagger

--- MinimalAPIPeliculas/MinimalAPIPeliculas/Program.cs
+++ MinimalAPIPeliculas/MinimalAPIPeliculas/Program.cs
@@ -1,5 +1,32 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+var origenesPermitidos = builder.Configuration.GetValue<string>("origenesPermitidos")!;
+
+//inicio del area de los servicios
+
+builder.Services.AddCors(opciones =>
+opciones.AddDefaultPolicy(configuracion =>
+{
+    configuracion.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+}));
+
+builder.Services.AddOutputCache();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//fin de area de los servicios
+
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors();
+app.UseOutputCache();
+
+//inicio de area de los middlware
 
 app.MapGet("/", () => "Hello World!");
 
