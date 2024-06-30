11d65e32a548b2faa05a659a844b2fa2968041b9
Leandro Bertero <leandro.bertero@hotmail.com>
2024-06-30 00:05:06 -0300

    actualizacion de la maxima longitud de caracteres en el tipo de documento

--- Proyecto2024.Server/Proyecto2024.BD/Data/Entity/TDocumento.cs
+++ Proyecto2024.Server/Proyecto2024.BD/Data/Entity/TDocumento.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Proyecto2024.BD.Data.Entity
 {
+    [Index(nameof(Codigo), Name = "TDocumento_UQ", IsUnique = true)]
     public class TDocumento : EntityBase
     {
+        //Mensaje para el dato del Código
+        //Mensaje para el usuario desde el backEnd hacia el frontEnd
+        [Required(ErrorMessage = "El código del tipo de documento es obligatorio")]
+        [MaxLength(4, ErrorMessage = "Máximo número de caracteres {1}.")]
         public string? Codigo { get; set; }
+
+
+
+        //Mensaje para el dato del Nombre
+        //Mensaje para el usuario desde el backend hacia el frontEnd
+        [Required(ErrorMessage = "El nombre del tipo de documento es obligatorio")]
+        [MaxLength(4, ErrorMessage = "Máximo número de caracteres {1}.")]
         public string? Nombre { get; set; }
     }
 }
---
+++ Proyecto2024.Server/Proyecto2024.BD/Migrations/20240630030419_MaximaLongitudDeDocumento.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proyecto2024.BD.Migrations
+{
+    /// <inheritdoc />
+    public partial class MaximaLongitudDeDocumento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "TDocumentos",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Codigo",
+                table: "TDocumentos",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "TDocumento_UQ",
+                table: "TDocumentos",
+                column: "Codigo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "TDocumento_UQ",
+                table: "TDocumentos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "TDocumentos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Codigo",
+                table: "TDocumentos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+        }
+    }
+}
