5470939578bd0a58a6d3a67e37a5131d84555d8c
Leandro Bertero <leandro.bertero@hotmail.com>
2024-07-05 21:34:42 -0300

    Mensajes para el usuario en la tabla Usuarios

--- Proyecto_LaGran7.Server/Proyecto2024.BD/Data/Entity/Usuarios.cs
+++ Proyecto_LaGran7.Server/Proyecto2024.BD/Data/Entity/Usuarios.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,21 @@ using System.Threading.Tasks;
 namespace ProyectoLaGran7.BD.Data.Entity
 {
     [Index(nameof(ReservaId), Name = "Usuario_UQ", IsUnique = true)]
-    [Index(nameof(Nombre), nameof(Mail), nameof(Contrasenia), nameof(Rol), Name = "Persona_Nombre_Mail_Rol", IsUnique = false)]
+    [Index(nameof(Nombre), nameof(Mail), nameof(Contraseña), nameof(Rol), Name = "Persona_Nombre_Mail_Rol", IsUnique = false)]
     public class Usuarios : EntityBase
     {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El E-mail es obligatorio")]
         public string? Mail { get; set; }
-        public string? Contrasenia { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string? Contraseña { get; set; }
+        [Required(ErrorMessage = "El rol es obligatorio")]
         public int? Rol { get; set; }
 
         //relaciones entre tablas
+        [Required(ErrorMessage = "El número de reserva es obligatorio")]
         public int ReservaId { get; set; }
 
         public Reserva? Reserva { get; set; }
