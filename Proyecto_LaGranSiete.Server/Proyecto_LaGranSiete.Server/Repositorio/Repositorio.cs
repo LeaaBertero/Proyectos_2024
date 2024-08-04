99af0a1857c91e1ae39ce3a924a4de741d52e7d6
Leandro Bertero <leandro.bertero@hotmail.com>
2024-08-04 13:54:46 -0300

    se generar un error cuando quiero hacer el repositorio del método Post de la tabla Reservas

--- Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Server/Repositorio/Repositorio.cs
+++ Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Server/Repositorio/Repositorio.cs
@@ -5,26 +5,41 @@ using Proyecto_LaGranSiete.BD.Data;
 using Proyecto_LaGranSiete.BD.Data.Entity;
 using Proyecto_LaGranSiete.Shared.DTO;
 
-//namespace Proyecto_LaGranSiete.Server.Repositorio
-//{
-//    //classe especializada en hacer un crud sobre una tabla cualquiera de la base de datos
-//    public class Repositorio<E> : where E : class, IEntityBase //(E = Entidad) 
-//    {
-//        private readonly Context context;
+namespace Proyecto_LaGranSiete.Server.Repositorio
+{
+    //classe especializada en hacer un crud sobre una tabla cualquiera de la base de datos
+    //public class Repositorio<E> : where E : class, IEntityBase //(E = Entidad) 
+    //{
+    //    private readonly Context context;
 
-//        //constructor del repositorio
-//        public Repositorio(Context context) //Inyección de dependencia (Context (Base de datosS) ) 
-//        {
-//            //Haciendo control + . (se crea y se asigna el campo context)
+    //    //constructor del repositorio
+    //    public Repositorio(Context context) //Inyección de dependencia (Context (Base de datosS) ) 
+    //    {
+    //        //Haciendo control + . (se crea y se asigna el campo context)
 
-//            this.context = context;
-//        }
+    //        this.context = context;
+    //    }
 
-//        public async Task<List<E>> Select() //Task == "Tarea"
-//        {
-//            return await context.Set<E>().ToListAsync();
-//        }
-//    }
+    //    public async Task<List<E>> Select() //Task == "Tarea"
+    //    {
+    //        return await context.Set<E>().ToListAsync();
+    //    }
+    //}
 
-   
-//}
+    //public async Task<int> Insert(E entidad)
+    //{
+    //    try
+    //    {
+    //        //---------------------------------------------------------------------------- //Comentar acá despues de hacer la inyección en el context
+    //        await context.Set<E>().Add(entidad);
+    //        await context.SaveChangesAsync(); //espera y guarda los cambios del context
+    //        return entidad.Id; //Id de la entidad 
+    //    }
+    //    catch (Exception ErrorMessage)
+    //    {
+    //        throw ErrorMessage;
+    //    }
+    //}
+}
+
+
