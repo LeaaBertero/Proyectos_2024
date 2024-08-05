275fa13a63da20440d2402e47c46d34a8a85acb3
Leandro Bertero <leandro.bertero@hotmail.com>
2024-08-05 15:32:50 -0300

    Primera tabla

---
+++ Biblioteca.Server/Biblioteca.BD/Data/Entity/Editorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.BD.Data.Entity
+{
+    public class Editorial : EntityBase
+    {
+        public string? EditorialNombre { get; set; }
+        public string? EditorialMail { get; set; }
+        public int? EditorialTelefono { get; set; }
+
+        public string? EditorialDomicilio { get; set; }
+        public int IdLocalidad { get; set; }
+    }
+}
+
+
+
--- Biblioteca.Server/Biblioteca.Server/Program.cs
+++ Biblioteca.Server/Biblioteca.Server/Program.cs
@@ -1,3 +1,6 @@
+using Biblioteca.BD.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//conexion con la base de datos
+builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=conn"));
+
 
 var app = builder.Build();
 
