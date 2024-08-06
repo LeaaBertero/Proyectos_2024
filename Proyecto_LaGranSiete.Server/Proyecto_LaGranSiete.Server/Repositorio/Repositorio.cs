1f1cd4b44b3d07ea087ad6e0e00f0eaa22936960
Leandro Bertero <leandro.bertero@hotmail.com>
2024-08-05 22:18:58 -0300

    repositorio - Inicio

--- Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Server/Repositorio/Repositorio.cs
+++ Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.Server/Repositorio/Repositorio.cs
@@ -6,36 +6,37 @@ using Proyecto_LaGranSiete.BD.Data.Entity;
 
 namespace Proyecto_LaGranSiete.Server.Repositorio
 {
-    //public class Repositorio<E> : where E : class, IEntityBase
-    //{
-    //    private readonly Context context;
+    public class Repositorio<E> : where E : class, IEntityBase
+    {
+        private readonly Context context;
 
-    //    public Repositorio(Context context)
-    //    {
-    //        this.context = context;
-    //    }
+        public Repositorio(Context context)
+        {
+            this.context = context;
+        }
 
-    //    public async Task<List<E>> Select() //Task == "Tarea"
-    //    {
-    //       return await context.Set<E>().ToListAsync();
-    //    }
-    //}
+        public async Task<List<E>> Select() //Task == "Tarea"
+        {
+            return await context.Set<E>().ToListAsync();
+        }
 
-    //public async Task<int> Insert(E entidad)
-    //{
-    //    try
-    //    {
-    //        //Usuarios entidad = Mapper.Map<Usuarios>(entidadDTO);
+        public async Task<int> Insert(E entidad)
+        {
+            try
+            {
+                //Usuarios entidad = Mapper.Map<Usuarios>(entidadDTO);
 
-    //        await context.Set<E>().add(entidad);
-    //        await context.SaveChangesAsync(); //espera y guarda los cambios del context
-    //        return entidad.Id; //Id de la entidad 
-    //    }
-    //    catch (Exception Err)
-    //    {
-    //        //return BadRequest(ErrorMessage.Message);
-    //        throw;
-    //    }
-    //}
-
+                context.Set<E>().Add(entidad);
+                await context.SaveChangesAsync(); //espera y guarda los cambios del context
+                return entidad.Id; //Id de la entidad 
+            }
+            catch (Exception Err)
+            {
+                //return BadRequest(ErrorMessage.Message);
+                throw;
+            }
+        }
+    }
 }
+
+
