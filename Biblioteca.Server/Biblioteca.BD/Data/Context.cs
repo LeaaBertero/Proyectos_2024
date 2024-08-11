a2e99004362cd43281029ee2cfd0d615f0ea8b3e
Leandro Bertero <leandro.bertero@hotmail.com>
2024-08-11 16:02:07 -0300

    //codigo que evita que un registro de la base de datos, pueda borrarse en cascada

--- Biblioteca.Server/Biblioteca.BD/Data/Context.cs
+++ Biblioteca.Server/Biblioteca.BD/Data/Context.cs
@@ -27,5 +27,21 @@ namespace Biblioteca.BD.Data
         {
 
         }
+
+        //codigo que evita que un registro de la base de datos, pueda borrarse en cascada
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Éste codigo sirve para evitar que se borren los datos en cascada en la base de datos
+            var cascadeFKs = modelBuilder.Model.G­etEntityTypes()
+                                            .SelectMany(t => t.GetForeignKeys())
+                                            .Where(fk => !fk.IsOwnership
+                                                        && fk.DeleteBehavior == DeleteBehavior.Casca­de);
+            foreach (var fk in cascadeFKs)
+            {        //Elimina el cmportamiento               
+                fk.DeleteBehavior = DeleteBehavior.Restr­ict;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
