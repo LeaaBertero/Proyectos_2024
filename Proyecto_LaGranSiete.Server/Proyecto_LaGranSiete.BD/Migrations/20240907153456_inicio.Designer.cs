5752ca22653d33c41ceb9029e2b46f38dab9f83b
Leandro Bertero <leandro.bertero@hotmail.com>
2024-09-07 12:35:40 -0300

    actualizo BD

--- Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.BD/Migrations/20240907153456_inicio.Designer.cs
+++ Proyecto_LaGranSiete.Server/Proyecto_LaGranSiete.BD/Migrations/20240907153456_inicio.Designer.cs
@@ -12,7 +12,7 @@ using Proyecto_LaGranSiete.BD.Data;
 namespace Proyecto_LaGranSiete.BD.Migrations
 {
     [DbContext(typeof(Context))]
-    [Migration("20240907153116_inicio")]
+    [Migration("20240907153456_inicio")]
     partial class inicio
     {
         /// <inheritdoc />
